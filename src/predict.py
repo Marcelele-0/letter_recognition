ad0d78c1e25406dbc736071719b6e645ed46c67b
marcelele-0 <mnem12321@gmail.com>
2025-04-03 23:25:56 +0200

    feat: update .gitignore to include specific MNIST data directory and add new image files for training

--- src/predict.py
+++ src/predict.py
@@ -11,7 +11,7 @@ from torch.nn import Sequential, Conv2d, Linear, ReLU, Flatten, BatchNorm2d, Max
 # Fix for OpenMP error
 os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
 
-CHECKPOINT_PATH = "checkpoints/epoch=9-step=8750.ckpt"
+CHECKPOINT_PATH = "wandb_logs/MNIST-Training/wzsrz3qv/checkpoints/epoch=9-step=8750.ckpt"
 DATA_DIR = "data/my_data/"
 
 def load_model(checkpoint_path):
@@ -28,8 +28,6 @@ def get_transform():
         transforms.Lambda(lambda img: ImageOps.autocontrast(img)),
         transforms.Resize((28, 28)),
         transforms.Lambda(lambda img: ImageOps.invert(img)),
-        transforms.RandomRotation(degrees=20),
-        transforms.RandomAffine(degrees=0, translate=(0.1, 0.1)),
         transforms.ToTensor(),
         transforms.Normalize((0.1307,), (0.3081,))
     ])
